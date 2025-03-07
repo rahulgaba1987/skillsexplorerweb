ec01d5754601f56bd600b0910e5dbc48fa19f958
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-07 16:26:34 +0530

    About us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Courses from "./pages/Courses";
 import PageNotFound from "./pages/PageNotFound";
 import CustomNavBar from "./pages/CustomNavBar";
+import Videos from "./pages/Videos";
+import AboutUs from "./pages/AboutUs";
+import AboutUs from "./pages/ContactUs";
 function App() {
   return (
     <>
@@ -11,6 +14,10 @@ function App() {
         <Routes>
           <Route  path="/" element={<CustomNavBar/>}/>
           <Route  path="/courses" element={<Courses/>}/>
+         
+          <Route  path="/videos" element={<Videos/>}/>
+          <Route  path="/about" element={<AboutUs/>}/>
+          <Route  path="/contact" element={<ContactUs/>}/>
           <Route  path="*" element={<PageNotFound/>}/>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Videos extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>This is my videos Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Videos
