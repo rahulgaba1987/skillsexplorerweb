1b8791f07300ccc8c06276831f83a609e6254047
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-07 15:47:54 +0530

    added al files

---
+++ src/pages/CustomNavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const CustomNavBar = () => {
+  return (
+    <div>
+         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Skills Explorer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Courses</Nav.Link>
+            <Nav.Link href="#pricing">Videos</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">About US</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Contact US
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default CustomNavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomNavBar from './CustomNavBar'
+
+function Home() {
+  return (
+
+        <CustomNavBar/>
+  )
+}
+
+export default Home
