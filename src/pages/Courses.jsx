b5653d40ba3af7de1c638c3f2cfe20ad2acd47e9
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 13:48:25 +0530

    added courses

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
 
 function Courses() {
   return (
     <div className="main">
-      <h1>My Courses</h1>
+      <div className="courses">
+        <div>
+          <h3>Core Java</h3>
+          <ul>
+            <li>JVM,JDK & JRE</li>
+            <li>Data Types</li>
+            <li>Operators</li>
+            <li>Control Statements</li>
+            <li>OOps Concepts</li>
+            <li>Immutable & Mutable String</li>
+            <li>Constructor In Java</li>
+            <li>Interface </li>
+            <li>Abstract Class</li>
+            <li>Inheritance</li>
+            <li>Static,Super & this keyword</li>
+            <li>Polymorphism</li>
+            <li>Collection Framework</li>
+            <li>Multi Threading</li>
+            <li>JDBC</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Core Java</h3>
+          <ul>
+            <li>JVM,JDK & JRE</li>
+            <li>Data Types</li>
+            <li>Operators</li>
+            <li>Control Statements</li>
+            <li>OOps Concepts</li>
+            <li>Immutable & Mutable String</li>
+            <li>Constructor In Java</li>
+            <li>Interface </li>
+            <li>Abstract Class</li>
+            <li>Inheritance</li>
+            <li>Static,Super & this keyword</li>
+            <li>Polymorphism</li>
+            <li>Collection Framework</li>
+            <li>Multi Threading</li>
+            <li>JDBC</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Core Java</h3>
+          <ul>
+            <li>JVM,JDK & JRE</li>
+            <li>Data Types</li>
+            <li>Operators</li>
+            <li>Control Statements</li>
+            <li>OOps Concepts</li>
+            <li>Immutable & Mutable String</li>
+            <li>Constructor In Java</li>
+            <li>Interface </li>
+            <li>Abstract Class</li>
+            <li>Inheritance</li>
+            <li>Static,Super & this keyword</li>
+            <li>Polymorphism</li>
+            <li>Collection Framework</li>
+            <li>Multi Threading</li>
+            <li>JDBC</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Courses
+export default Courses;
