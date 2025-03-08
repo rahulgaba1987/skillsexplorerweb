c529b44e223af0f7536798619cd74c14f055549a
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 23:28:58 +0530

    aboit us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,11 +6,18 @@ function AboutUs() {
       <div class="container  about">
         <div class="about-section">
             <h1 className='text-center'>Welcome to Our Team</h1>
-            <p>We are dedicated to delivering the best solutions for our clients. Our team is passionate about creating innovative and impactful results in everything we do.</p>
+            <p>we are passionate about empowering individuals and teams with the knowledge and skills to excel in today’s fast-paced technology landscape. Whether you're a beginner eager to dive into the world of coding or a seasoned professional looking to sharpen your skills, we provide comprehensive training to help you achieve your goals.</p>
+            <ul>
+                <li><strong>Programming Languages:</strong> Java,Hibernate,Spring Core, Spring Boot, Microservices</li>
+                <li><strong>Web Development:</strong> HTML, CSS, React, Angular,Java Script</li>
+                <li><strong>Database Management:</strong> SQL, NoSQL, MongoDB, PostgreSQL.</li>
+                <li><strong>Project Training</strong></li>
+                <li><strong>Industry Exposure</strong></li>
+            </ul>
         </div>
         <div className="team">
             <div>
-                <img src="https://via.placeholder.com/120" alt="Team Member"/>
+                {/* <img src={pic} alt="Team Member" height={200} width={200}/> */}
                 <h3>Rahul Gaba</h3>
                 
             </div>
