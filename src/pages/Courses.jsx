7a1cc209d44546a06a6506072f395d5835eda227
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 17:10:18 +0530

    courses

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -7,9 +7,10 @@ import JavaImage from '../asset/java.png';
 function Courses() {
 const [open, setOpen] = useState(false);
   return (
-    <div className="main">
+    <>
+    <div className="main courses">
 
-<Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem'}}>
       <Card.Img variant="top" src={JavaImage} />
       <Card.Body>
         <Card.Title>Core Java</Card.Title>
@@ -17,45 +18,94 @@ const [open, setOpen] = useState(false);
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
+       
         <Button
+        variant="success"
+         className="text-center"
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
         aria-expanded={open}
+
       >
         Core Java
       </Button>
+   
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={JavaImage} />
+      <Card.Body>
+        <Card.Title>Core Java</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       
+        <Button
+        variant="success"
+         className="text-center"
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+
+      >
+        Core Java
+      </Button>
+   
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={JavaImage} />
+      <Card.Body>
+        <Card.Title>Core Java</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+       
+        <Button
+        variant="success"
+         className="text-center"
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+
+      >
+        Core Java
+      </Button>
+   
       </Card.Body>
     </Card>
         
-      <Collapse in={open}>
-        <div id="example-collapse-text">
-            <br/>
-          <ul>
-
-            <li>JVM,JDK & JRE</li>
-            <li>Data Types</li>
-            <li>Operators</li>
-            <li>Control Statements</li>
-            <li>OOps Concepts</li>
-            <li>Immutable & Mutable String</li>
-            <li>Constructor In Java</li>
-            <li>Interface </li>
-            <li>Abstract Class</li>
-            <li>Inheritance</li>
-            <li>Static,Super & this keyword</li>
-            <li>Polymorphism</li>
-            <li>Collection Framework</li>
-            <li>Multi Threading</li>
-            <li>JDBC</li>
-          </ul>
-        </div>
-      </Collapse>
-      <div className="courses">
-        
-          
-          
-      </div>
+      
+      
     </div>
+    <Collapse in={open}>
+    <div id="example-collapse-text">
+        <br/>
+      <ul>
+
+        <li>JVM,JDK & JRE</li>
+        <li>Data Types</li>
+        <li>Operators</li>
+        <li>Control Statements</li>
+        <li>OOps Concepts</li>
+        <li>Immutable & Mutable String</li>
+        <li>Constructor In Java</li>
+        <li>Interface </li>
+        <li>Abstract Class</li>
+        <li>Inheritance</li>
+        <li>Static,Super & this keyword</li>
+        <li>Polymorphism</li>
+        <li>Collection Framework</li>
+        <li>Multi Threading</li>
+        <li>JDBC</li>
+      </ul>
+    </div>
+  </Collapse>
+  </>
   );
 }
 
