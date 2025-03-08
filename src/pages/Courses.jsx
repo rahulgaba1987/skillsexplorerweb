f88f54da79352a44fdbbc036098873644540f95d
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 16:48:44 +0530

    Core java course Card

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
+import Card from 'react-bootstrap/Card';
+import JavaImage from '../asset/java.png';
 function Courses() {
 const [open, setOpen] = useState(false);
   return (
     <div className="main">
+
+<Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={JavaImage} />
+      <Card.Body>
+        <Card.Title>Core Java</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
         <Button
         onClick={() => setOpen(!open)}
         aria-controls="example-collapse-text"
@@ -13,10 +24,14 @@ const [open, setOpen] = useState(false);
       >
         Core Java
       </Button>
+      </Card.Body>
+    </Card>
+        
       <Collapse in={open}>
         <div id="example-collapse-text">
-        <h3>Core Java</h3>
+            <br/>
           <ul>
+
             <li>JVM,JDK & JRE</li>
             <li>Data Types</li>
             <li>Operators</li>
