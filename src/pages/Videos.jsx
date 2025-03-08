f72e2e654088af5db59285d11971efd43c470a28
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 13:05:32 +0530

    Added videos

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,7 +4,19 @@ export class Videos extends PureComponent {
   render() {
     return (
       <div className="main">
-        <h1>This is my videos Page</h1>
+        <div className='videos'>
+        <div className='text-center'>
+                <h1>Core Java</h1>
+                <iframe  src="https://www.youtube.com/embed/videoseries?si=8VXBQFjFEC0_qCOw&amp;list=PLL9sLyyoOiI4voOoojAa94Gg1T3dD8hze" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
+        
+            <div className='text-center'>
+                <h1>Java 8 Features</h1>
+                <iframe  src="https://www.youtube.com/embed/videoseries?si=lp1duT5yzdsNyC9l&amp;list=PLL9sLyyoOiI6irJbG1npEzWPbwbkCn0bW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
+        
+        </div>
+        
       </div>
     )
   }
