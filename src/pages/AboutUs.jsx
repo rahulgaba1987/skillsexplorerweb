696ca7d1b7c4d257aa0cf43e6a7c04306c6556fa
Rahul Gaba <rahulgaba4u1987@gmail.com>
2025-03-08 16:19:04 +0530

    Added Pages

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function AboutUs() {
   return (
     <div className="main">
-      <h1>This is About Us page</h1>
+      <div class="container  about">
+        <div class="about-section">
+            <h1 className='text-center'>Welcome to Our Team</h1>
+            <p>We are dedicated to delivering the best solutions for our clients. Our team is passionate about creating innovative and impactful results in everything we do.</p>
+        </div>
+        <div className="team">
+            <div>
+                <img src="https://via.placeholder.com/120" alt="Team Member"/>
+                <h3>Rahul Gaba</h3>
+                
+            </div>
+           
+        </div>
+    </div>
     </div>
   )
 }
